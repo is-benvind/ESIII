9ee4ffa2fec5e684cc21b27d811f4c07ee028cf5
Israel Benvindo <75487136+is-benvind@users.noreply.github.com>
2023-10-05 17:26:40 -0300

    Delete atividae3 directory

--- atividae3/Pedido.java
+++
@@ -1,4 +0,0 @@
-Porque o princípio "Tell, don't ask" diz que testes e validações devem existir dentro
-da própria classe, o que vai contra um forte acoplamento, em que uma ou mais classes
-possuem uma forte dependência entre si.
-
