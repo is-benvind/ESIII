cd4c9ba9b18791712b56babace2039e52fc995dc
Israel Benvindo <75487136+is-benvind@users.noreply.github.com>
2023-10-05 17:22:55 -0300

    Add files via upload

---
+++ atividade02/questao03/src/Compra.java
@@ -0,0 +1,12 @@
+public class Compra {
+    private double valorCompra;
+
+    public void realizarCompra() {
+        // lógica de compra
+    }
+
+    public void atualizarSaldo() {
+        // atualizar saldo do cliente
+    }
+
+}
---
+++ atividade02/questao03/src/Conta.java
@@ -0,0 +1,4 @@
+public class Conta{
+    private String numeroConta;
+    private double saldo;
+}
